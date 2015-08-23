9f908412800f3162d16130d7b4598e93721c7018
Nicht Jens <nichtjens@gmail.com>
2015-08-24 01:02:50 +0200

    added example for recognizer, added docstrings for all examples

--- example.py
+++ example.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
-from bastard import recognized
+from bastard import recognized, recognizer
 
 
 class A(object):
+    """The base class"""
     def f(self):
         print 'A.f'
 
 
 class S(A):
+    """A sub class using super()"""
     def f(self):
         super(S, self).f()
         print 'S.f'
@@ -16,13 +18,23 @@ class S(A):
 
 @recognized
 class B(A):
+    """A sub class using self.parent"""
     def f(self):
         self.parent.f()
         print 'B.f'
 
 
+@recognizer('mother')
+class C(A):
+    """A sub class using mother as parent name"""
+    def f(self):
+        self.mother.f()
+        print 'C.f'
+
+
 
 S().f()
 B().f()
+C().f()
 
 
