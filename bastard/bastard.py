ffc63f9c3e3cbe4e954c54a6fc7f3c5a28cfe198
Nicht Jens <nichtjens@gmail.com>
2015-08-24 01:01:18 +0200

    added further decorator that allows to choose the parent name

--- bastard/bastard.py
+++ bastard/bastard.py
@@ -1,5 +1,15 @@
 
-def recognized(cls):
-    setattr(cls, 'parent', property(lambda self: super(cls, self)))
+def recognized(cls, parent="parent"):
+    setattr(cls, parent, property(lambda self: super(cls, self)))
     return cls
+
+
+class recognizer(object):
+
+    def __init__(self, parent):
+        self.parent = parent
+
+    def __call__(self, cls):
+        return recognized(cls, self.parent)
+
 
